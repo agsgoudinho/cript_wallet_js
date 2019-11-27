af97d950f141b0d0222144dee7c4eb5696487936
agsgoudinho <aguinaldo@alunos.utfpr.edu.br>
2019-11-27 02:05:58 -0300

    Conexao websocket basica funcionando

---
+++ Peer.js
@@ -0,0 +1,52 @@
+const net = require("net");
+
+module.exports = class Peer {
+
+    constructor(port) {
+        this.port = port;
+        this.connections = [];
+
+        const server = net.createServer((socket) => {
+            this.onSocketConnected(socket)
+        });
+
+        server.listen(port, () => console.log("Ouvindo porta " + port))
+    }
+
+    connectTo(address) {
+        if (address.split(":").length !== 2)
+            throw Error("O endereço do outro peer deve ser composto por host:port ");
+
+        const [host, port] = address.split(":");
+
+        const socket = net.createConnection({ port, host }, () =>
+            this.onSocketConnected(socket)
+        );
+    }
+
+    onSocketConnected(socket) {
+        this.connections.push(socket);
+
+        socket.on('data', (data) =>
+            this.onData(socket, data)
+        );
+
+        socket.on('close', () => {
+            this.connections = this.connections.filter( conn => {
+                return conn !== socket;
+            })
+        });
+
+        this.onConnection(socket);
+    }
+
+    onData(socket, data) {
+        console.log("received: ", data.toString())
+    }
+
+    onConnection(socket) { }
+    
+    broadcast(data) {
+        this.connections.forEach(socket => socket.write(data))
+    }
+}
